15bfc1f5e3a8e8fbe2cc9a807b323155f08e3970
Alessia Lacitignola <alelaci21@gmail.com>
2023-02-07 22:04:18 +0100

    BE-Week5-Esercizio2

---
+++ BE-Week5-Esercizio2/src/main/java/it/epicode/numero1/beans/StatoOrdine.java
@@ -0,0 +1,7 @@
+package it.epicode.numero1.beans;
+
+public enum StatoOrdine {
+	IN_CORSO,
+	PRONTO,
+	SERVITO
+}
---
+++ BE-Week5-Esercizio2/src/test/java/it/epicode/numero1/BeWeek5Esercizio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.numero1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeWeek5Esercizio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
